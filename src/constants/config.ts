bd65cac2e6f424aff5b8efb5904e336fc8ba653c
Nguyen Huu Thuat <thuatnguyen09112002@gmail.com>
2025-01-13 20:45:00 +0700

    docker setup

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,16 +1,19 @@
+import { config } from 'dotenv'
 import argv from 'minimist'
 
 const options = argv(process.argv.slice(2))
+export const isProduction = Boolean(options.env === 'production')
 
-console.log(options)
+console.log('options', options)
+console.log('isProduction', isProduction)
+console.log('options.env', options.env)
 
-export const isProduction = Boolean(options.production)
-
-console.log(isProduction)
-console.log(options.env)
+config({
+  path: options.env ? `.env.${options.env}` : '.env'
+})
 
 export const envConfig = {
-  port: process.env.PORT as string,
+  port: (process.env.PORT as string) || 4000,
   host: process.env.HOST as string,
   clientUrl: process.env.CLIENT_URL as string
 }
